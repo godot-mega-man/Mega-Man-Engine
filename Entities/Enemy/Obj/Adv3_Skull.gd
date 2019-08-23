f54cf33c819b9d04b9dbc5c31289c6d837b316df
Firstject <plotrunn343@gmail.com>
2019-08-23 18:26:09 +0700

    Made skull splits 2 fireballs on each side

--- Entities/Enemy/Obj/Adv3_Skull.gd
+++ Entities/Enemy/Obj/Adv3_Skull.gd
@@ -6,7 +6,15 @@ var flameburst_fireball = preload("res://Entities/Enemy/Obj/MM6_FlameBurstFireba
 
 func _on_Skull_taken_damage(value, target, player_proj_source) -> void:
 	for i in number_of_fireball:
-		var fireball = flameburst_fireball.instance()
+		var fireball
+		
+		fireball = flameburst_fireball.instance()
 		get_parent().add_child(fireball)
 		fireball.global_position = global_position
-		fireball.bullet_behavior.angle_in_degrees = rand_range(-200, 20)
+		fireball.bullet_behavior.angle_in_degrees = rand_range(-200, -110)
+		
+		fireball = flameburst_fireball.instance()
+		get_parent().add_child(fireball)
+		fireball.global_position = global_position
+		fireball.bullet_behavior.angle_in_degrees = rand_range(-70, 20)
+		
