b2e5af9b4a191f031d51eb10f8bb07d2b6fe236a
Snowflakes <plotrunn343@gmail.com>
2021-04-30 21:24:40 +0700

    Add autoload UnfocusVolumeLowerer.gd

---
+++ Project/Src/Autoload/UnfocusVolumeLowerer.gd
@@ -0,0 +1,26 @@
+# Lowers the volume automatically when the windows is not focused, and turn
+# back on if focused.
+
+extends Node
+
+
+const VOLUME_dB_LOWER = -15.0
+const VOLUME_dB_NORMAL = 0.0
+
+
+var disabled : bool
+
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_WM_FOCUS_IN:
+		reset()
+	if what == NOTIFICATION_WM_FOCUS_OUT:
+		lower()
+
+
+func lower():
+	AudioServer.set_bus_volume_db(0, VOLUME_dB_LOWER)
+
+
+func reset():
+	AudioServer.set_bus_volume_db(0, VOLUME_dB_NORMAL)
